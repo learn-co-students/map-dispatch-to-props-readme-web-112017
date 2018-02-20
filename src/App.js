a9fbc2dc2d46be42bb9a84527b8917ddb0d95d06
ddbrennan <ddbren@gmail.com>
2018-02-19 19:54:50 -0500

    added dispatch to props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
-import { addItem } from  './actions/items';
+import { bindActionCreators } from 'redux'
 
 class App extends Component {
 
   handleOnClick() {
-    this.props.store.dispatch(addItem());
+    this.props.addItem();
   }
 
   render() {
@@ -26,5 +26,11 @@ const mapStateToProps = (state) => {
     items: state.items
   };
 };
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+      addItem: addItem
+    }, dispatch);
+}
 
 export default connect(mapStateToProps)(App);
